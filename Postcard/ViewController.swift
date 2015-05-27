3caba57ecc9645401598f506e8dd7c191fab786f
Shivendra Singh <shivendrasingh@Shivendras-MacBook-Air.local>
2015-05-27 23:29:23 +1000

    Added Functionality to Name text field

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
     @IBOutlet weak var mailButton: UIButton!
+    @IBOutlet weak var nameLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,7 +35,17 @@ class ViewController: UIViewController {
         enterMessageTextField.text = ""
         enterMessageTextField.resignFirstResponder()
         
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextField.text
+        nameLabel.textColor = UIColor.blueColor()
+        
+        enterNameTextField.text = ""
+        enterNameTextField.resignFirstResponder()
+        //enterNameTextField.hidden = true
+        
+        
         mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
+        mailButton.setTitleColor(UIColor.redColor(), forState: UIControlState.Normal)
     }
 
 }
